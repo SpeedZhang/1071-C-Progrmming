8f00ba1caa7a18b6d95d02dd77cedfd6d1f3eca2
SpeedZhang <43335357+SpeedZhang@users.noreply.github.com>
2018-09-18 16:32:20 +0800

    Update Leapyear.cpp

--- w02/Leapyear.cpp
+++ w02/Leapyear.cpp
@@ -1,1 +1,14 @@
+#include<stdio.h>
 
+int main(){
+
+    int year;
+    printf("Enter year :");
+    scanf("%d", &year);
+0    if(year%4==0 &&year%100!=0||year%400==0){
+    	printf("year %d is a leap year\n",year);
+    }else{
+    	printf("year %d is not a leap year\n",year);
+	}
+	    return 0;
+} 
