13844ab87f0a5f0c5b336a70f47b3815c81a8611
SpeedZhang <43335357+SpeedZhang@users.noreply.github.com>
2018-09-26 10:34:43 +0800

    Update BMI.cpp

--- w03/BMI.cpp
+++ w03/BMI.cpp
@@ -8,34 +8,29 @@ int main (){
 	scanf("%lf",&KG);
     BMI=KG/(M*M);
 while(KG>=0&&M>=0){
-	if(BMI<18.5){
-		printf("your BMI is: %.1lf \n",BMI);
+    printf("your BMI is: %.1lf \n",BMI);
+	if(BMI<18.5){	
 		printf("your體重過輕\n");
 	}
 	else if(BMI>=18.5&&BMI<24){
-		printf("your BMI is: %.1lf \n",BMI);
 		printf("your體重正常\n");
 	}
 	else if(BMI>=24&&BMI<27){
-		printf("your BMI is: %.1lf \n",BMI);
 		printf("your體重過重\n");
 	} 
 	else if(BMI>=27&&BMI<30){
-		printf("your BMI is: %.1lf \n",BMI);
 		printf("your體重輕度肥胖\n");
 	}
 	else if(BMI>=30&&BMI<35){
-		printf("your BMI is: %.1lf \n",BMI);
 		printf("your體重中度肥胖\n");
 	}
 	else if(BMI>=35){
-		printf("your BMI is: %.1lf \n",BMI);
 		printf("your體重重度肥胖\n");
 }
 	printf("Enter your 身高(M):");
-	scanf("%1f",&M);
+	scanf("%lf",&M);
 	printf("Enter your 體重(KG)");
-	scanf("%1f",&KG);
+	scanf("%lf",&KG);
 	
 }
 }
