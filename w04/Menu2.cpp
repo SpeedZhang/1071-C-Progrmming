c364ead1131823dfbfab94a01053c947fb2c6eee
SpeedZhang <43335357+SpeedZhang@users.noreply.github.com>
2018-10-03 01:02:38 +0800

    Update Menu2.cpp

--- w04/Menu2.cpp
+++ w04/Menu2.cpp
@@ -1,50 +1,174 @@
 #include<stdio.h>
-
-int main()
+void prchar(char c,int n)
 {
-int choice;
-while(1)
-{
-        printf("Main Menu\n");
-        printf("1. Rectangle\n");
-        printf("2. Vertical Triangle\n");
-        printf("3. Exit\n");
-        printf("=> ");
-        scanf("%d", &choice);
-        if(choice==3)break;
-switch(choice)
-{
-case 1:
-void prchar(char c, int n)
-{
-        for(int i=1;i<=n;i++)
+    for(int i=1;i<=n;i++)
+    {
         printf("%c",c);
+    }
 }
 void drawRect(int height,int width,int filled)
 {
+    if(filled)
+    {
+        for(int i=1;i<=height;i++)
+        {
+            prchar('*',width);
+            printf("\n");
+        }
+        printf("\n");
+    }
+    else
+    {
+        for(int i=1;i<=height;i++)
+        {
+            for(int j=1;j<=width;j++)
+            {
+                if(i==1||i==height||j==1||j==width)
+                    printf("*");
+                    else
+                    printf(" ");
+            }
+            printf("\n");
+        }
+    }
+}
+void drawVertTri(int height,int type,int filled)
+{
+    switch(type)
+    {
+    case 1:
         if(filled)
         {
-          for(int i=1;i<=height;i++)
-          {
-             printf('*',width);
-             printf("\n");
-          }else
-           {
             for(int i=1;i<=height;i++)
-             {
-              if(i==1||i==height||j==1||j==width)
-                   printf("*");
-                   else
-                   printf(" ");
-             }
+            {
+                prchar('*',i);
                 printf("\n");
-           }
+            }
         }
+        else
+        {
+            for(int i=1;i<=height;i++)
+            {
+                if(i==1||i==height)
+                {
+                    prchar('*',i);
+                }
+                else
+                {
+                    printf("*");
+                    prchar(' ',i-2);
+                    printf("*");
+                }
+                printf("\n");
+            }
+        }
+        break;
+    case 2:
+        if(filled)
+        {
+            for(int i=1;i<=height;i++)
+            {
+                    prchar(' ',height-i+1);
+                    prchar('*',i);
+                    printf("\n");
+            }
+        }
+        else
+        {
+            for(int i=1;i<=height;i++)
+                {
+            	for(int j=1;j<=height;j++)
+            	     {
+            		if(i+j==height+1||i==height||j==height)
+            		    printf("*");
+            		else
+					    printf(" ");
+				     }
+				printf("\n");
+			    }
+        }
+        break;
+    case 3:
+        if(filled)
+        {
+            for(int i=1;i<=height;i++)
+            {
+                prchar('*',height-i+1);
+                printf("\n");
+            }
+        }
+            else
+            {
+                for(int i=1;i<=height;i++)
+                    {
+            	   for(int j=1;j<=height;j++)
+            	        {
+            		if(i+j==height+1||i==1||j==1)
+            		    printf("*");
+            		else
+					    printf(" ");
+				        }
+				printf("\n");
+			        }
+            }
+        break;
+    case 4:
+        if(filled)
+        {
+            for(int i=1;i<=height;i++)
+            {
+                    prchar(' ',i);
+                    prchar('*',height-i+1);
+                    printf("\n");
+            }
+        }
+            else
+            {
+                for(int i=1;i<=height;i++)
+                {
+            	for(int j=1;j<=height;j++)
+                    {
+            		if(i==j||i==1||j==height)
+            		    printf("*");
+            		else
+					    printf(" ");
+				    }
+				printf("\n");
+			    }
+            }
+        break;
+    }
 }
-
-
-
-
-}
-}
+int main()
+{
+    int choice;
+    int height;
+    int width;
+    int filled;
+    int type;
+    while(1)
+    {
+        printf("Main Menu\n");
+        printf("1. Rectangle\n");
+        printf("2. Vertical Triangle\n");
+        printf("3. Tsosceles Triangle\n");
+        printf("4. Exit\n");
+        printf("=> ");
+        scanf("%d", &choice);
+        if(choice==4)break;
+        switch(choice)
+        {
+        case 1:
+            printf("Enter height, width, filled: ");
+            scanf("%d %d %d",&height,&width,&filled);
+            drawRect(height,width,filled);
+            break;
+        case 2:
+            printf("Enter height, type, filled: ");
+            scanf("%d %d %d",&height,&type,&filled);
+            drawVertTri(height,type,filled);
+            break;
+        }
+    }
+    return 0;
 }
