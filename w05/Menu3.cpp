a0ee5c1a71f2ca14506a2e918b62e856b3433574
SpeedZhang <43335357+SpeedZhang@users.noreply.github.com>
2018-10-08 18:05:23 +0800

    Update Menu3.cpp

--- w05/Menu3.cpp
+++ w05/Menu3.cpp
@@ -1,1 +1,51 @@
-
+#include<stdio.h>
+int isLeap(int year)
+{
+    if(year%4==0 &&year%100!=0||year%400==0)
+        return 1;
+    else
+        return 0;
+}
+int timeconvert(int hh,int mm,int ss)
+{
+    return ss=(hh*3600)+(mm*60)+ss;
+}
+int main ()
+{
+    int choice;
+    int year;
+    int hh,mm,ss;
+while(1)
+    {
+    printf(" Main Menu \n");
+    printf(" 1.Leapyear \n");
+    printf(" 2.Second \n");
+    printf(" 3.Exit \n");
+    printf(" => ");
+    scanf("%d",&choice);
+    if(choice==3)
+        break;
+    switch(choice)
+    {
+    case 1:
+        printf("Enter year :");
+        scanf("%d",&year);
+        if(isLeap(year))
+        {
+        printf("year %d is a leap year\n",year);
+        }
+        else
+        {
+        printf("year %d is not a leap year\n",year);
+        }
+        break;
+    case 2:
+        printf("Enter time hh:mm:ss=");
+        scanf("%d:%d:%d",&hh,&mm,&ss);
+        int seconds=timeconvert(hh,mm,ss);
+        printf("%d %d %d = %d seconds\n",hh,mm,ss,seconds);
+        break;
+    }
+}
+return 0;
+}
