d8bcb91cc260e86e8d0f62d5c84872b6535bb731
SpeedZhang <43335357+SpeedZhang@users.noreply.github.com>
2018-10-20 11:56:58 +0800

    Update sor.cpp

--- w06/sor.cpp
+++ w06/sor.cpp
@@ -3,34 +3,39 @@ void ori(int m[],int n)
 {
     for(int i=0;i<n;i++)
     {
-        printf("%2d ",m[i]);
+        printf("%3d",m[i]);
     }
     printf("\n");
 }
-void sor(int m[],int n,int t)
+void sor(int m[],int n)
 {
     for(int i=0;i<n;i++)
     {
-        for(int j=0;j<n;j++)
-        {
-            if(m[j]>m[j+1])
+        int tp;
+            for(int j=0;j<n-1;j++)
             {
-                t=m[j+1];
+                if(m[j]>m[j+1])
+                {
+                tp=m[j+1];
                 m[j+1]=m[j];
-                m[j]=t;
+                m[j]=tp;
+                }
             }
-        }
-            printf("%2d ",m[i]);
+    }
+    for(int k=0;k<n;k++)
+    {
+    printf("%3d",m[k]);
     }
 }
+
 int main()
 {
     int m[10]={34,78,12,9,87,66,88,99,56,11};
     int n=10;
-    int t,j;
+    int t;
  printf("original: ");
  ori(m,n);
  printf("sorting: ");
- sor(m,n,t);
+ sor(m,n);
  return 0;
 }
