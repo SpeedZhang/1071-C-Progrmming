20d5c6a37a04aeaabc26767042f3ff84e3ec1416
SpeedZhang <43335357+SpeedZhang@users.noreply.github.com>
2018-10-23 18:10:54 +0800

    Update gcd.cpp

--- w07/gcd.cpp
+++ w07/gcd.cpp
@@ -1,11 +1,13 @@
 #include<stdio.h>
-void gcd(int n,int k)
+int gcd(int n,int k)
 {
     int gcd;
-    if(n!=0)
-    k=k%n;
-    else if(n==0)
-    printf("gcd(48,126) = %d\n",gcd);
+        for(int i=1;i<=n;i++)
+        {
+           if(n%i==0 && k%i==0)
+            gcd=i;
+        }
+    return gcd;
 }
 int rgcd(int n,int k)
 {
@@ -18,5 +20,14 @@ int main()
 {
     int n,k;
     printf("rgcd(48,126) = %d\n",rgcd(48,126));
-    gcd(48,126);
+    printf("gcd(48,126) = %d\n",gcd(48,126));
+
+    printf("rgcd(48,126) = %d\n",rgcd(48,128));
+    printf("gcd(48,126) = %d\n",gcd(48,128));
+
+    printf("rgcd(48,126) = %d\n",rgcd(48,144));
+    printf("gcd(48,126) = %d\n",gcd(48,144));
+
+    printf("rgcd(48,126) = %d\n",rgcd(48,48));
+    printf("gcd(48,126) = %d\n",gcd(48,48));
 }
