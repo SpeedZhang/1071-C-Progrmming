c08922d91b06387c42006d5bbf6d669f01465824
SpeedZhang <43335357+SpeedZhang@users.noreply.github.com>
2018-10-30 18:11:33 +0800

    Create t2_P2_id.cpp

---
+++ w08/t2_P2_id.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#define MAX 100
+int main()
+{
+int n1,n2;
+char cont='Y';
+int data[MAX];
+int occurrences[10];
+    while(cont=='Y')
+    {
+    printf("Enter n1 and n2: ");
+    scanf("%d %d",&n1,&n2);
+    }
+}
