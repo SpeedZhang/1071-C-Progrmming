4b418c0b481872e4b97943cd9fff41dc1fd34006
SpeedZhang <43335357+SpeedZhang@users.noreply.github.com>
2018-11-06 17:32:14 +0800

    Update lotto.cpp

--- w09/lotto.cpp
+++ w09/lotto.cpp
@@ -13,11 +13,17 @@ struct Lotto
     Date date;
     int num[6];
 };
+int duplicate(int num[],int pos)
+{
+    for(int i=0;i<pos;i++)
+    {
+        if(num[i]==num[pos])
+            return 1;
+    }
+    return 0;
+}
 int  main()
 {
-    Date date;
-    Date dates[MAX];
-
     Lotto lotto;
     Lotto lottos[MAX];
 
@@ -32,15 +38,18 @@ int  main()
          lottos[i].date.year=rand()%11+2010;
          lottos[i].date.month=rand()%12+1;
          lottos[i].date.day=rand()%31+1;
-         printf("%d/%02d/%02d\n",lottos[i].date.year,lottos[i].date.month,lottos[i].date.day);
+         printf("%d/%02d/%02d  ",lottos[i].date.year,lottos[i].date.month,lottos[i].date.day);
 
         for(int j=0;j<6;j++)
         {
             do
             {
                 temp=rand()%49+1;
-                printf("%02d",lottos[i].num[j]);
             }
+            while(duplicate(lottos[i].num,j));
+                lottos[i].num[j]=temp;
+                printf("%02d ",lottos[i].num[j]);
         }
+        printf("\n");
     }
 }
